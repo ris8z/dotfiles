dca4f57067bfe2bb4482aad402356b3659278b7f
ris8z <giuseppe592999@gmail.com>
2026-06-13 15:26:57 +0100

    harper to avoid grammar erros

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,15 @@
+-----------------------------------------------------------------
+-- general
+-----------------------------------------------------------------
+
 vim.loader.enable()
 
--- Basics
+-- general: basics
 vim.o.wrap = false
 vim.g.mapleader = " "
 
--- Indentation
+
+-- general: indentation
 vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.expandtab = true
@@ -12,19 +17,21 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 
--- Quality of life
+
+-- general: quality of life
 vim.o.incsearch = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.winborder = "rounded"
 vim.opt.conceallevel = 1
 vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
-vim.o.updatetime = 250      -- Rende l'interfaccia più reattiva (default 4000)
-vim.o.timeoutlen = 300      -- Rende le combo di tasti (leader key) più scattanti
-vim.o.lazyredraw = true     -- Non ridisegna lo schermo durante le macro (più veloce)
-vim.o.ttyfast = true        -- Dice a nvim che hai un terminale veloce
+vim.o.updatetime = 250      
+vim.o.timeoutlen = 300      
+vim.o.lazyredraw = true     
+vim.o.ttyfast = true        
 
--- Swap and Backup and Undofile
+
+-- general: swap and backup and undo file
 vim.o.swapfile = false
 vim.o.backup = false
 vim.o.writebackup = false
@@ -36,11 +43,12 @@ end
 vim.opt.undofile = true
 vim.opt.undodir = undodir
 
+
+-----------------------------------------------------------------
+-- plugins list 
 -----------------------------------------------------------------
 
--- List of installed Plugins
 vim.pack.add({
-    { src = "https://github.com/rebelot/kanagawa.nvim" },
     { src = "https://github.com/catppuccin/nvim" },
     { src = "https://github.com/nvim-mini/mini.pick" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
@@ -50,8 +58,16 @@ vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false,                            build = ":TSUpdate" }
 })
 
--- Require Plugins and setting plugins
+
+-----------------------------------------------------------------
+-- setup 
+-----------------------------------------------------------------
+
+-- setup: mini
 require("mini.pick").setup()
+
+
+-- setup: Tree-sitter
 require("nvim-treesitter.configs").setup(
     {
         ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "bash", "java" },
@@ -62,7 +78,11 @@ require("nvim-treesitter.configs").setup(
         }
     }
 )
+
+-- setup: mason
 require("mason").setup()
+
+-- setup: blink and LSP
 require("blink.cmp").setup({
     completion = {
         menu = {
@@ -72,6 +92,15 @@ require("blink.cmp").setup({
     fuzzy = { implementation = "lua" }
 })
 
+vim.lsp.enable(
+    {
+        "markdown_oxide",
+        "lemminx",
+        "harper_ls",
+    }
+)
+
+-- setup: Obsidian 
 local DIR_2BRAIN = "/home/esposigg/Documents/projects/2brain"
 if not vim.g._obsidian_setup_done then
     vim.g._obsidian_setup_done = true
@@ -96,18 +125,11 @@ if not vim.g._obsidian_setup_done then
 end
 
 
--- LSPs enable and config
-vim.lsp.enable(
-    {
-        "markdown_oxide",
-        "lemminx",
-    }
-)
-
+-----------------------------------------------------------------
+-- keymap
 -----------------------------------------------------------------
 
--- Keymaps
-
+-- keymap: general
 vim.keymap.set("n", "<leader>w", ":write<CR>")
 vim.keymap.set("n", "<leader>e", ":e .<CR>")
 vim.keymap.set("n", "<leader>l", ":e!<CR>")
@@ -122,6 +144,7 @@ vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
 vim.keymap.set("n", "<leader>g", ":Pick grep_live<CR>")
 vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
 
+-- keymap: Obsidian
 vim.keymap.set("n", "<leader>oo", ":cd " .. DIR_2BRAIN .. "<CR>")
 vim.api.nvim_create_autocmd("User", {
     pattern = "ObsidianNoteEnter",
@@ -130,44 +153,26 @@ vim.api.nvim_create_autocmd("User", {
             buffer = ev.buf,
             desc = "Follow link",
         })
-        vim.keymap.set("n", "<leader>on", ":Obsidian template note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>") -- convert note to template and remove leading white space
-        vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")                                     -- stripe date from title and replace - with space (must have cursor on the title)
+        vim.keymap.set("n", "<leader>on", ":Obsidian template note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>") -- Convert note to template and remove leading whitespace
+        vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")                                     -- Stripe date from title and replace - with space (must have cursor on the title)
         vim.keymap.set("n", "<leader>ok", ":!mv '%:p' " .. DIR_2BRAIN .. "/4_main<CR>:bd<CR>")                        -- move to main folder
         vim.keymap.set("n", "<leader>odd", ":!rm '%:p'<CR>:bd<CR>")                                                   -- remove this note
     end,
 })
 
+-- keymap: LSP
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { desc = "LSP code actions" })
 
--- Vim cmd
--- vim.cmd("colorscheme kanagawa")
+
+-----------------------------------------------------------------
+-- Theme
+-----------------------------------------------------------------
+
 vim.cmd("colorscheme catppuccin")
 vim.cmd(":hi statusline guibg=NONE")
-
--- small function to open the selected text on firefox
-function OpenSelectionInFirefox()
-    local _, start_line, start_col, _ = unpack(vim.fn.getpos("'<"))
-    local _, end_line, end_col, _ = unpack(vim.fn.getpos("'>"))
-    local lines = vim.fn.getline(start_line, end_line)
-
-    if #lines == 0 then return end
-    lines[#lines] = string.sub(lines[#lines], 1, end_col)
-    lines[1] = string.sub(lines[1], start_col)
-
-    local selection = table.concat(lines, " ")
-    selection = selection:gsub("^%s*(.-)%s*$", "%1")
-    if selection == "" then return end
-
-    local url = selection
-    if not selection:match("^https?://") then
-        url = "https://www.google.com/search?q=" .. vim.fn.escape(selection, " ")
-    end
-
-    vim.fn.jobstart({ "firefox", url }, { detach = true })
-end
-
-vim.api.nvim_set_keymap('v', '<leader>s', [[:lua OpenSelectionInFirefox()<CR>]], { noremap = true, silent = true })
